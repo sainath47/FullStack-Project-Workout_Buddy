bf05f8c065b067c2ecc020da33fb01498d345014
sainath47 <reddysainath47@gmail.com>
2023-03-29 14:31:12 +0530

    connection should be established before the listen

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
-require('dotenv').config()
-const path = require('path')
-const express = require('express');
-const workoutRoutes = require('./routes/workout.routes')
-const userRoutes = require('./routes/user.routes')
-const mongoose = require('mongoose')
+require("dotenv").config();
+const path = require("path");
+const express = require("express");
+const workoutRoutes = require("./routes/workout.routes");
+const userRoutes = require("./routes/user.routes");
+const mongoose = require("mongoose");
 //express app
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
 //routes
-app.use((req,res,next) => {
-    console.log(req.path, req.method);
-    next()
-})
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(
+    console.log("MongoDB connected"),
+    app.listen(process.env.PORT, () => {
+      console.log("listening on port 4000");
+    })
+  )
+  .catch((error) => {
+    console.log(error);
+  });
 
-app.use('/api/workouts',workoutRoutes)
-app.use('/api/user',userRoutes)
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use("/api/workouts", workoutRoutes);
+app.use("/api/user", userRoutes);
 
 app.use(express.static(path.join(__dirname, "./frontend/build")));
 app.get("*", function (req, res) {
@@ -27,12 +39,4 @@ app.get("*", function (req, res) {
   );
 });
 
-
-
 //listen for requests
-app.listen(process.env.PORT,()=>{console.log('listening on port 4000')
-
-mongoose.connect(process.env.MONGODB_URI).then(console.log('MongoDB connected')).catch((error)=>{console.log(error)})
-})
-
-
